f2d7c6916c1badbd660f8e44082d64dadb87ef25
viktorfa <vikfand@gmail.com>
2016-10-06 15:59:38 +0800

    Not use messenger library, but only pure express server.

--- app.js
+++ app.js
@@ -3,34 +3,8 @@
 const _ = require('lodash');
 const http = require('http');
 const express = require('express');
-const Bot = require('messenger-bot');
 const bodyParser = require('body-parser');
-
-let bot = new Bot({
-    token: process.env.PAGE_TOKEN,
-    verify: process.env.VERIFY_TOKEN,
-    app_secret: process.env.APP_SECRET
-});
-
-bot.on('error', (err) => {
-    console.log(err.message)
-});
-
-bot.on('message', (payload, reply) => {
-    let text = payload.message.text;
-
-    bot.getProfile(payload.sender.id, (err, profile) => {
-        if (err) throw err;
-
-        reply({text}, (err) => {
-                //if (err) throw err;
-
-                console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-            }
-        )
-    })
-});
-
+const request = require('request');
 
 let app = express();
 const PORT = process.env.PORT || 3000;
@@ -43,15 +17,86 @@ app.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
-app.get('/webhook/', (req, res) => {
-    return bot._verify(req, res)
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong FACEBOOK_SITE_TOKEN')
 });
 
-app.post('/webhook/', (req, res) => {
-    bot._handleMessage(req.body);
-    res.end(JSON.stringify({status: 'ok'}))
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i];
+        handleMessage(event);
+    }
+    res.sendStatus(200)
 });
 
+const handleMessage = (event) => {
+    let sender = event.sender.id;
+    if (isTextMessage(event)) {
+        handleTextMessage(event);
+    }
+};
+
+const handleTextMessage = (event) => {
+    typingOn(event.sender.id);
+    //sendMessage(getTextMessage('Spraydiaré'), event.sender.id);
+};
+
+const getTextMessage = (text) => {
+    return {text: text};
+};
+
+const sendMessageBlock = () => {
+
+};
+
+const typingOn = (senderId) => {
+    sendAction({sender_action: 'typing_on'}, senderId)
+};
+
+const sendMessage = (messageObject, senderId) => {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: senderId},
+            message: messageObject,
+        }
+    }, (error, response, body) => {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    });
+};
+
+const sendAction = (actionObject, senderId) => {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: senderId},
+            message: actionObject,
+        }
+    }, (error, response, body) => {
+        if (error) {
+            console.log('Error sending action: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    });
+};
+
+const isTextMessage = (event) => {
+    return event.message && event.message.text;
+};
 
 http.createServer(app).listen(PORT);
 console.log(`Listening to ${PORT}`);
